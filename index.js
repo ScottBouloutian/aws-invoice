2e34c18f906424045aac3804290b2d7abb9a4517
Scott Bouloutian <ScottBouloutian@gmail.com>
2017-04-01 10:30:56 -0400

    support filtering by non-user keys

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const prompt = require('prompt');
 const s3 = require('s3');
 const csvParse = require('csv-parse/lib/sync');
 const fs = require('fs');
 const mustache = require('mustache');
 const pdf = require('html-pdf');
+const ld = require('lodash');
 
 const client = s3.createClient();
 
-const listObjects = bucket => {
+const listObjects = (bucket) => {
     const lister = client.listObjects({
         s3Params: {
-            Bucket: bucket
-        }
+            Bucket: bucket,
+        },
     });
     let objects = [];
-    lister.on('data', data => objects = objects.concat(data.Contents));
+    lister.on('data', (data) => {
+        objects = objects.concat(data.Contents);
+    });
     return new Promise((resolve, reject) => {
         lister.on('error', error => reject(error));
         lister.on('end', () => resolve(objects));
@@ -26,8 +28,8 @@ const downloadFile = (bucket, file) => {
         localFile: 'data.csv',
         s3Params: {
             Bucket: bucket,
-            Key: file
-        }
+            Key: file,
+        },
     });
     return new Promise((resolve, reject) => {
         downloader.on('error', error => reject(error));
@@ -35,19 +37,20 @@ const downloadFile = (bucket, file) => {
     });
 };
 
-const generatePDF = html => {
+const generatePDF = (html) => {
     const options = { format: 'Letter' };
     return new Promise((resolve, reject) => (
-        pdf.create(html, options).toFile('invoice.pdf', error => error ? reject(error) : resolve())
+        pdf.create(html, options).toFile('invoice.pdf', error =>
+            (error ? reject(error) : resolve()))
     ));
 };
 
-const generate = options => {
-    const { bucket, year, month, key, value, sender, recipient, title } = options;
-    return listObjects(bucket).then(objects => {
+const generate = (options) => {
+    const { bucket, year, month, sender, recipient, title, resources } = options;
+    return listObjects(bucket).then((objects) => {
         const file = objects
             .map(object => object.Key)
-            .find(key => {
+            .find((key) => {
                 const match = key.match(/\d+-aws-cost-allocation-(\d{4})-(\d{2})\.csv/);
                 const paddedMonth = month.length === 1 ? `0${month}` : month;
                 return match && match[1] === year && match[2] === paddedMonth;
@@ -57,23 +60,28 @@ const generate = options => {
         const file = fs.readFileSync('data.csv', 'utf-8');
         fs.unlinkSync('data.csv');
         const fileData = file.substring(file.indexOf('\n') + 1);
-        const parsed = csvParse(fileData, {
-            auto_parse: true
-        });
+        const parsed = csvParse(fileData, { auto_parse: true });
         const columns = parsed[0];
-        const userKey = `user:${key}`;
         const columnLabels = [
             'ProductName',
             'ItemDescription',
             'UsageType',
             'UsageQuantity',
-            'TotalCost'
+            'TotalCost',
         ];
-        const columnIndices = columnLabels.map(label => columns.indexOf(label));
-        const userKeyIndex = columns.indexOf(userKey);
+        const columnIndices = columns.reduce((acc, val, index) => {
+            acc[val] = index;
+            return acc;
+        }, { });
         const data = parsed.slice(1)
-            .filter(row => row[userKeyIndex] === value)
-            .map(row => columnIndices.map(index => row[index]));
+            .filter(row => (
+                resources.some(resource => (
+                    ld.every(resource, (value, key) => (
+                        (row[columnIndices[key]] === value)
+                    ))
+                ))
+            ))
+            .map(row => columnLabels.map(label => row[columnIndices[label]]));
         const invoiceTemplate = fs.readFileSync('invoice.html', 'utf-8');
         const total = data
             .map(row => row[row.length - 1])
