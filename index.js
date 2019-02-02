c2cdc4d8322222d9c302f3cb71e9682d72247034
Scott Bouloutian <ScottBouloutian@gmail.com>
2019-02-02 17:55:45 -0500

    binary permission fixes

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const aws = require('aws-sdk');
 const Promise = require('bluebird');
+const { exec } = require('child_process');
 const { generate } = require('./lib/invoice');
 
 // Constant declarations
 const s3 = new aws.S3({ region: 'us-east-1' });
 const getObject = Promise.promisify(s3.getObject, { context: s3 });
 const bucket = process.env.AWS_INVOICE_BUCKET;
-
-process.env.PATH = `${process.env.PATH}:${process.env.LAMBDA_TASK_ROOT}/bin`;
+const execPromise = Promise.promisify(exec);
+const binPath = `${process.env.LAMBDA_TASK_ROOT}/bin`;
+process.env.PATH = `${process.env.PATH}:/tmp/bin`;
 
 // Download the configuration from s3
 function downloadConfig() {
@@ -19,7 +21,9 @@ function downloadConfig() {
 
 // Lambda interface
 exports.handler = (event, context, callback) => {
-    downloadConfig()
+    execPromise(`cp -r ${binPath} /tmp`)
+        .then(() => execPromise('chmod 755 /tmp/bin/wkhtmltopdf'))
+        .then(() => downloadConfig())
         .then(config => Promise.map(config.invoices, invoice => generate(invoice, config)))
         .then(() => callback(null, 'Done'))
         .catch(error => callback(error));
